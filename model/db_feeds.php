b1c352c0ea72b99df5965620c161d3151bd1ed2b
Rovun <rovun@foxmail.com>
2014-01-18 11:42:32 +0800

    Update db_feeds.php

--- model/db_feeds.php
+++ model/db_feeds.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: db_feeds.php 7 2011-09-20 15:02:20Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: db_feeds.php 7 2011-09-20 15:02:20Z rovun $ 
 
 
 class db_feeds extends spModel  
