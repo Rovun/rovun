07465c3bd5754e433b03226b9744e47bc32cb92c
Rovun <rovun@foxmail.com>
2014-01-18 10:49:19 +0800

    Update admin.php

--- controller/admin.php
+++ controller/admin.php
@@ -1,8 +1,8 @@
 <?php
 /////////////////////////////////////////////////////////////////
-//云边开源轻博, Copyright (C)   2010 - 2011  qing.thinksaas.cn 
-//EMAIL:nxfte@qq.com QQ:234027573                              
-//$Id: admin.php 34 2011-10-28 16:56:54Z anythink $ 
+//优云开源轻博, Copyright (C)   2010 - 2011  www.rovun.com 
+//EMAIL:rovun@foxmail.com     Q群:329354214                              
+//$Id: admin.php 34 2011-10-28 16:56:54Z rovun $ 
 
 
 class admin extends top
